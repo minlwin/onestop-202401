8d78d616a9e7c64756a68c7edb652576601dd542
Min Lwin <lwin.zawmin@gmail.com>
2024-07-02 14:38:31 +0630

    Update Sign In Page

--- hospital-web/src/app/pages/home/signin/signin.component.ts
+++ hospital-web/src/app/pages/home/signin/signin.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { WidgetsModule } from '../../../widgets/widgets.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [WidgetsModule, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './signin.component.html',
   styles: ``
 })
 export class SigninComponent {
 
+  form:FormGroup
+
+  constructor(builder:FormBuilder) {
+    this.form = builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 }
