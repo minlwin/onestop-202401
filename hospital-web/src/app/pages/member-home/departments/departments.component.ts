03601a8606e55d621da0ab27010a0f713b1efa53
Min Lwin <lwin.zawmin@gmail.com>
2024-07-04 15:09:43 +0630

    Update Top List Views

--- hospital-web/src/app/pages/member-home/departments/departments.component.ts
+++ hospital-web/src/app/pages/member-home/departments/departments.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { WidgetsModule } from '../../../widgets/widgets.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
   standalone: true,
-  imports: [],
+  imports: [WidgetsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './departments.component.html',
   styles: ``
 })
--- hospital-web/src/app/pages/member-home/members/patients/patients.component.ts
+++ hospital-web/src/app/pages/member-home/members/patients/patients.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { WidgetsModule } from '../../../../widgets/widgets.module';
 
 @Component({
   selector: 'app-patients',
   standalone: true,
-  imports: [],
+  imports: [WidgetsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './patients.component.html',
   styles: ``
 })
